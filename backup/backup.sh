b0f59cedb5221b5cbadfe5617139c383430697e3
timaba <tba+github@timaba.de>
2014-11-24 19:53:27 +0100

    Update backup.sh
    changed email in license

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,7 +2,7 @@
 #Backupskript - Backup with rsync and ssh
 #Version: 0.1.5 (2014-11-22)
 #
-#Copyright (C) 2014  Tilman Bartsch <tba@timaba.de>
+#Copyright (C) 2014  Tilman Bartsch <tba+github@timaba.de>
 #
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
